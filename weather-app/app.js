6b6059d419471f2a26225b2281d6d572598118b6
Kiril Zhelyazkov <knz140@aubg.edu>
2018-06-18 00:05:48 +0600

    initial commit. The user can set a desired location and the coordinates and full address will be returned

---
+++ weather-app/app.js
@@ -0,0 +1,25 @@
+const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+// argv.address is the user inputted address 
+request({
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(argv.address)}`,
+  json: true
+}, (error, response, body) => {
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+});
