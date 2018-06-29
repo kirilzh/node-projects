1d383e23a2e5ad25b66f0747a00ef211bfcf9ee3
Kiril Zhelyazkov <knz140@aubg.edu>
2018-06-29 14:16:43 +0600

    added async function testing with assertion library

--- tests/utils/utils.test.js
+++ tests/utils/utils.test.js
@@ -1,17 +1,43 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers', () => {
   var res = utils.add(33, 11);
 
-  if (res != 44){
-    throw new Error(`Expected 44, but got ${res}`);
-  }
+  expect(res).toBe(44).toBeA('number');
+});
+
+it('should async add two numbers', (done) => {
+  utils.asyncAdd(4, 3, (sum) => {
+    expect(sum).toBe(7).toBeA('number');
+    done();
+  });
 });
 
 it('should square a number', () => {
   var res = utils.square(4);
 
-  if (res != 16){
-    throw new Error(`Expected 16, but got ${res}`);
-  }
+  expect(res).toBe(16).toBeA('number');
+});
+
+it('should async square a number', (done) => {
+  utils.asyncSquare(5, (square) => {
+    expect(square).toBe(25).toBeA('number');
+    done();
+  });
+});
+
+it('should verify first and last name are', () => {
+  var user = {location: 'Stara Zagora', age: 22};
+  var res = utils.setName(user, 'Kiril Zhelyazkov');
+
+  expect(res).toInclude({
+    firstName: 'Kiril',
+    lastName: 'Zhelyazkov'
+  });
+});
+
+it('should expect some values', () => {
+  expect(12).toNotBe(11);
 });
