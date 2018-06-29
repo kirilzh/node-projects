17b4cfc2639708e245995d25fd608b4e30ba8fe4
Kiril Zhelyazkov <knz140@aubg.edu>
2018-06-29 15:39:54 +0600

    testing with spies

---
+++ tests/server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+var app = express();
+
+app.get('/', (req, res) => {
+  res.status(404).send({
+    error: 'Page not found',
+    name: 'Todo App v1.0'
+  })
+});
+
+app.get('/users', (req, res) => {
+  res.send([{
+    name: 'Mike',
+    age: 7
+  }, {
+    name: 'Andrew',
+    age: 25
+  }, {
+    name: 'Jen',
+    age: 25
+  }]);
+})
+
+app.listen(3000);
+
+module.exports.app = app;
