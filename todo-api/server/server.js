8342a71648a11757c52a5cf2db97c6dc1eb10697
kirilzh <kiril.zhelyazkov9@gmail.com>
2018-07-12 16:24:47 +0300

    setup app for heroku

--- todo-api/server/server.js
+++ todo-api/server/server.js
@@ -7,6 +7,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -43,13 +44,29 @@ app.get('/todos/:id', (req, res) => {
     }
 
     res.send({todo});
+  }).catch((e) => {app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo){
+      return res.status(404).send();
+    }
+
+    res.send({todo});
   }).catch((e) => {
     res.status(400).send();
   })
 });
+    res.status(400).send();
+  })
+});
 
-app.listen(3000, () => {
-  console.log("started on port 3000");
+app.listen(port, () => {
+  console.log(`started on port ${port}`);
 });
 
 module.exports = {app};
