6f78a8f587af8801c78c73bf0616dc054653cb99
kirilzh <kiril.zhelyazkov9@gmail.com>
2018-07-13 20:09:37 +0300

    hash user password before saving

--- todo-api/playground/hashing.js
+++ todo-api/playground/hashing.js
@@ -1,37 +1,17 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abv');
-console.log(token);
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+  });
+});
 
-var decoded = jwt.verify(token, '123abv');
-console.log(decoded);
-// var message = 'I am user number 3';
-// var hash = SHA256(message).toString();
-//
-// console.log(`Message: ${message}`);
-// console.log(`Hash: ${hash}`);
-//
-// var data = {
-//   id: 4
-// };
-//
-// var token = {
-//   data,
-//   hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
-// }
-//
-// // token.data.id = 5;
-// // token.hash = SHA256(JSON.stringify(token.data)).toString();
-//
-// var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
-//
-// if (resultHash === token.hash){
-//   console.log('Data was not changed');
-// } else {
-//   console.log('Data was changed. Do not trust! ');
-// }
+var hashedPassword = "$2a$10$g6Svs5PIBWINvMd.LDJiGeD4/QVa8mDdlyRmpQUUf1wfIQKnH6J0.";
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+})
