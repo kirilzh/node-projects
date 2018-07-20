35c7adc4374e685faa753f5d9da9e0fbdcda9397
kirilzh <kiril.zhelyazkov9@gmail.com>
2018-07-20 22:46:48 +0300

    greet new user and alert others

--- chat-app/public/js/index.js
+++ chat-app/public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'browser',
-    text: 'sent from browser'
-  });
 });
 
 socket.on('disconnect', function() {
--- chat-app/server/server.js
+++ chat-app/server/server.js
@@ -15,13 +15,30 @@ io.on('connection', socket => {
   console.log('New user connected');
 
   socket.emit('newMessage', {
-    from: 'Kiril',
-    text: 'first message',
-    createdAt: new Date()
+    from: 'admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
   });
 
   socket.on('createMessage', message => {
     console.log('browse message: ', message);
+    // io.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
+
+    socket.broadcast.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
   socket.on('disconnect', () => {
     console.log('User disconnected');
