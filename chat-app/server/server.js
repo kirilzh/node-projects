43efcb76a2105e1b1c7899114673080b8b45b039
kirilzh <kiril.zhelyazkov9@gmail.com>
2018-07-22 19:42:31 +0300

    add mustache.js fro message templates

--- chat-app/server/server.js
+++ chat-app/server/server.js
@@ -15,31 +15,34 @@ app.use(express.static(publicPath));
 io.on('connection', socket => {
   console.log('New user connected');
 
-  socket.emit('newMessage', generateMessage('admin', 'Welcome to chat app'));
+  socket.emit(
+    'newMessage',
+    generateMessage('Admin', 'Welcome to the chat app')
+  );
 
   socket.broadcast.emit(
     'newMessage',
-    generateMessage('admin', 'New user logged')
+    generateMessage('Admin', 'New user joined')
   );
 
   socket.on('createMessage', (message, callback) => {
-    console.log('browse message: ', message);
+    console.log('createMessage', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
     callback();
   });
 
   socket.on('createLocationMessage', coords => {
     io.emit(
-      'createLocationMessage',
-      generateLocationMessage('admin', coords.lat, coords.lon)
+      'newLocationMessage',
+      generateLocationMessage('Admin', coords.latitude, coords.longitude)
     );
   });
 
   socket.on('disconnect', () => {
-    console.log('User disconnected');
+    console.log('User was disconnected');
   });
 });
 
 server.listen(port, () => {
-  console.log(`Server is running on ${port}`);
+  console.log(`Server is up on ${port}`);
 });
